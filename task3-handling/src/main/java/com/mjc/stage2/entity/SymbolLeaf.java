bc92d087aae83288c0185a2bc4f33133db11858c
Mikhail <shaltynovm@gmail.com>
2023-09-29 14:59:23 +0530

    finished tasks 3

--- task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
+++ task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
@@ -1,8 +1,33 @@
 package com.mjc.stage2.entity;
 
-public class SymbolLeaf {
+public class SymbolLeaf extends  AbstractTextComponent{
 
     private char value;
+
+    public SymbolLeaf(char value, TextComponentType componentType) {
+        super(componentType);
+        this.value=value;
+    }
+
+    @Override
+    public String operation() {
+        return String.valueOf(value);
+    }
+
+    @Override
+    public void add(AbstractTextComponent textComponent) {
+throw new RuntimeException();
+    }
+
+    @Override
+    public void remove(AbstractTextComponent textComponent) {
+        throw new RuntimeException();
+    }
+
+    @Override
+    public int getSize() {
+        throw new RuntimeException();
+    }
 
     // Write your code here!
 }
--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,7 +1,18 @@
 package com.mjc.stage2.parser;
 
 
-public class WordParser {
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.SymbolLeaf;
+import com.mjc.stage2.entity.TextComponentType;
+
+public class WordParser extends AbstractTextParser {
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        char[] charArray = string.toCharArray();
+        for (char element : charArray) {
+            abstractTextComponent.add(new SymbolLeaf(element, TextComponentType.WORD));
+        }
+    }
 
     // Write your code here!
 
